641d2089aa9cc84a84872252c0e566f764fcb478
garlicxd <gergelypeti02@gmail.com>
2024-03-12 01:38:34 +0100

    layer with german, danish and hungarian characters added as SPE

--- keyboards/kprepublic/cstc40/keymaps/garlicxd/README.md
+++ keyboards/kprepublic/cstc40/keymaps/garlicxd/README.md
@@ -1,1 +1,137 @@
 # CSTC40 SNTH Layout
+
+Switches: Gateron Black  
+Keycaps: XDA
+
+## Layout
+
+#### Default Layer
+SNTH https://www.reddit.com/r/KeyboardLayouts/comments/18jefux/snth/
+
+|      |     |     |     |     | x   | x   |     |     |     |     |     |
+| ---- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| X    | P   | D   | M   | Q   |     |     | &   | Y   | O   | U   | '   |
+| S    | N   | T   | H   | V   |     |     | G   | C   | A   | E   | I   |
+| F    | B   | K   | L   | J   |     |     | Z   | W   | "   | ,   | .   |
+| PScr | GUI | TAB | R   | ENT |     |     | BSP | SPC | DEL | ESC |     |
+|      |     |     |     |     |     |     |     |     |     |     |     |
+|      |     | FUN | NAV | CAP |     |     | MOU | SYM | SPE |     |     |
+
+#### Default Caps Layer
+|     |     |     |     |     | x   | x   |     |     |     |     |     |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| X   | P   | D   | M   | Q   |     |     | #   | Y   | O   | U   | @   | 
+| S   | N   | T   | H   | V   |     |     | G   | C   | A   | E   | I   |
+| F   | B   | K   | L   | J   |     |     | Z   | W   | \\  | \`  | ~   |
+|     |     | _   | R   | -   |     | x   | BSP | SPC | DE  |     |     |
+
+#### Symbols Layer
+|     |     |     |     |     | x   | x   |     |     |     |     |     |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| 1   | 2   | 3   | 4   | %   |     |     | $   | 6   | 7   | 8   | 9   |
+| <   | [   | {   | (   | 5   |     |     | 0   | =   | /   | !   | ?   |
+| -   | +   | ^   | :   | *   |     |     | \|  | )   | }   | ]   | >   |
+|     |     | .   | ;   | ,   |     | x   | BSP | SPC | DEL |     |     |
+|     |     |     |     |     |     |     |     |     |     |     |     |
+|     |     |     | NAV |     |     |     |     |     |     |     |     |
+
+#### Nav Layer
+|     |     |     |     |     | x   | x   |     |     |     |     |     |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+|     | A+T | UP  | C+T | INS |     |     |     |     |     |     |     |
+| HOM | LFT | DWN | RGT | END |     |     |     | ENT | SHF | CTR | ALT |
+| UND | CPY | CUT | PST | RDO |     |     |     |     |     |     |     |
+|     |     |     |     |     | x   |     | BSP | SPC | DEL |     |     |
+A+T : Alt + Tab
+C+T: Ctrl + Tab
+Ctrl + Up Combo
+Ctrl + Down Combo
+
+#### Function Layer
+|     |     |     |     |     | x   | x   |     |     |     |     |     |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| F1  | F2  | F3  | F4  |     |     |     |     |     | RGB | RBT | BOT |
+| F5  | F6  | F7  | F8  |     |     |     |     |     |     |     |     |
+| F9  | F10 | F11 | F12 |     |     |     |     |     |     |     |     |
+|     |     |     |     |     |     |     |     |     |     |     |     |
+
+#### Special Layer
+|     |     |     |     |     | x   | x   |     |     |     |     |     |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+|     |     |     |     |     |     |     | ő   | ö   | ó   | ø   | ß   |
+|     |     |     |     |     |     |     | å   | ä   | á   | é   | í   |
+|     |     |     |     |     |     |     | ű   | ü   | ú   |     |     |
+|     |     |     |     | CAP |     | x   |     |     |     |     |     |
+
+#### Special Caps Layer
+|     |     |     |     |     | x   | x   |     |     |     |     |     |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+|     |     |     |     |     |     |     | Ő   | Ö   | Ó   | Ø   | ẞ   |
+|     |     |     |     |     |     |     | Å   | Ä   | Á   | É   | Í   |
+|     |     |     |     |     |     |     | Ű   | Ü   | Ú   |     |     |
+|     |     |     |     |     | x   | x   |     |     |     |     |     |
+
+#### QWERTY Layer
+|       |      |     |     |     | x   | x   |     |     |     |      |       |
+| ----- | ---- | --- | --- | --- | --- | --- | --- | --- | --- | ---- | ----- |
+| Tab   | Q    | W   | E   | R   | T   | Y   | U   | I   | O   | P    | Bksp  |
+| Esc   | A    | S   | D   | F   | G   | H   | J   | K   | L   | ;    | "     |
+| Shift | Z    | X   | C   | V   | B   | N   | M   | ,   | .   | /    | Enter |
+| BLTog | Ctrl | Alt | ENT | GUI | DEF | DEF | BSP | SPC | DEL | Ctrl | Alt   |
+
+#### League Layer
+|     |     |     |     |       | x   | x   |     |     |     |     |     |
+| --- | --- | --- | --- | ----- | --- | --- | --- | --- | --- | --- | --- |
+| 1   | 2   | 3   | 4   | 5     |     |     |     |     |     |     |     |
+| Q   | W   | E   | R   | Ctrl  |     |     |     |     |     |     |     |
+| 6   | 7   | D   | F   | Shift |     |     |     |     |     |     |     |
+|     |     | TAB | SPC | Chat  | DEF | DEF |     |     |     |     |     |
+
+#### Chat Layer
+Same as [[#Default Layer]], but enter switches back to [[#League Layer]].
+
+## Characters
+
+#### Basic Symbols
+
+> [!NOTE]- All basic symbols in order of importance in Rust
+> / _ : ( ) , . { } = ; " > \` ' - & < ! [ ] # | * \ ~ $ ? + ^ @ %
+
+> [!NOTE] Things that are important to type with numbers.
+> % ^ ( ) [ ] { } < > = + - * / : ~ 
+> Exclusive: % ^ ~
+
+> [!NOTE] Things that are important to program.
+> ( ) [ ] { } < > & | = + - * / : ! ? " \
+> Exclusive: & |
+
+> [!NOTE] Important to both
+> < > = + - * / :
+
+> [!NOTE] Important to neither
+> @ # $ \`
+
+#### Unicodes
+| Char | Shifted | Keycode | Value  | SValue |
+| ---- | ------- | ------- | ------ | ------ |
+| ß    | ẞ       | U_SS    | 0x00DF | 0x1E9E |
+| á    | Á       | U_A1    | 0x00E1 | 0x00C1 |
+| ä    | Ä       | U_A2    | 0x00E4 | 0x00C4 |
+| å    | Å       | U_A3    | 0x00E5 | 0x00C5 |
+| æ    | Æ       | U_AE    | 0x00E6 | 0x00C6 |
+| é    | É       | U_E1    | 0x00E9 | 0x00C9 |
+| í    | Í       | U_I1    | 0x00ED | 0x00CD |
+| ó    | Ó       | U_O1    | 0x00F3 | 0x00D3 |
+| ö    | Ö       | U_O2    | 0x00F6 | 0x00D6 |
+| ő    | Ő       | U_O3    | 0x0151 | 0x0150 |
+| ø    | Ø       | U_O4    | 0x00F8 | 0x00D8 |
+| ú    | Ú       | U_U1    | 0x00FA | 0x00DA |
+| ü    | Ü       | U_U2    | 0x00FC | 0x00DC |
+| ű    | Ű       | U_U3    | 0x0171 | 0x0170 |
+
+## Diary
+
+The practice was done with https://www.keybr.com/.
+
+1. 10.5 wpm
+2. 15 wpm
--- keyboards/kprepublic/cstc40/keymaps/garlicxd/keymap.c
+++ keyboards/kprepublic/cstc40/keymaps/garlicxd/keymap.c
@@ -25,23 +25,118 @@ enum custom_layers {
     _NAVIGATION,
     _SYMBOLS,
     _SPECIAL,
-    _QWERTY
+    _SPECIAL_CAPS,
+    _QWERTY,
+    _LEAGUE,
+    _CHAT
 };
-
 enum custom_keycodes {
     LLOCK = SAFE_RANGE,
+    CHAT,
+    ACHAT,
+    ENTnLEA
 };
+
+// combos
+enum combo_events {
+  JUMP_UP,
+  JUMP_DOWN,
+};
+const uint16_t PROGMEM ctrl_up[] = {KC_LCTL, KC_UP, COMBO_END};
+const uint16_t PROGMEM ctrl_down[] = {KC_LCTL, KC_DOWN, COMBO_END};
+combo_t key_combos[] = {
+  [JUMP_UP] = COMBO_ACTION(ctrl_up),
+  [JUMP_DOWN] = COMBO_ACTION(ctrl_down),
+};
+
+// unicode
+enum unicode_names {
+    SS,
+    AA,
+    AAA,
+    AAAA,
+    AE,
+    EE,
+    II,
+    OO,
+    OOO,
+    OOOO,
+    OOOOO,
+    UU,
+    UUU,
+    UUUU,
+    CSS,
+    CAA,
+    CAAA,
+    CAAAA,
+    CAE,
+    CEE,
+    CII,
+    COO,
+    COOO,
+    COOOO,
+    COOOOO,
+    CUU,
+    CUUU,
+    CUUUU
+};
+const uint32_t PROGMEM unicode_map[] = {
+    [SS] = 0x00DF, // ß
+    [AA] = 0x00E1, // á
+    [AAA] = 0x00E4, // ä
+    [AAAA] = 0x00E5, // å
+    [AE] = 0x00E6, // æ
+    [EE] = 0x00E9, // é
+    [II] = 0x00ED, // í
+    [OO] = 0x00F3, // ó
+    [OOO] = 0x00F6, // ö
+    [OOOO] = 0x0151, // ő
+    [OOOOO] = 0x00F8, // ø
+    [UU] = 0x00FA, // ú
+    [UUU] = 0x00FC, // ü
+    [UUUU] = 0x0171, // ű
+    [CSS] = 0x1E9E, // ẞ
+    [CAA] = 0x00C1, // Á
+    [CAAA] = 0x00C4, // Ä
+    [CAAAA] = 0x00C5, // Å
+    [CAE] = 0x00C6, // Æ
+    [CEE] = 0x00C9, // É
+    [CII] = 0x00CD, // Í
+    [COO] = 0x00D3, // Ó
+    [COOO] = 0x00D6, // Ö
+    [COOOO] = 0x0150, // Ő
+    [COOOOO] = 0x00D8, // Ø
+    [CUU] = 0x00DA, // Ú
+    [CUUU] = 0x00DC, // Ü
+    [CUUUU] = 0x0170, // Ű
+};
+
+#define U_SS UP(SS, CSS)
+#define U_A1 UP(AA, CAA)
+#define U_A2 UP(AAA, CAAA)
+#define U_A3 UP(AAAA, CAAAA)
+#define U_AE UP(AE, CAE)
+#define U_E1 UP(EE, CEE)
+#define U_I1 UP(II, CII)
+#define U_O1 UP(OO, COO)
+#define U_O2 UP(OOO, COOO)
+#define U_O3 UP(OOOO, COOOO)
+#define U_O4 UP(OOOOO, COOOOO)
+#define U_U1 UP(UU, CUU)
+#define U_U2 UP(UUU, CUUU)
+#define U_U3 UP(UUUU, CUUUU)
 
 // default layer keys
 #define TAB_SYM LT(_FUNCTION, KC_TAB)
 #define KCR_NAV LT(_NAVIGATION, KC_R)
-#define ENT_MOU LT(_MOUSE, KC_ENT)
-#define BSP_CAP LT(_DEFAULT_CAPS, KC_BSPC)
+#define ENT_CAP LT(_DEFAULT_CAPS, KC_ENT)
+#define BSP_MOU LT(_MOUSE, KC_BSPC)
 #define SPC_SYM LT(_SYMBOLS, KC_SPC)
 #define DEL_SPE LT(_SPECIAL, KC_DEL)
 
 // additional layer keys
 #define SCL_NAV LT(_NAVIGATION, KC_SCLN)
+#define ENT_SCA LT(_SPECIAL_CAPS, KC_ENT)
 
 #define L_DEF TO(_DEFAULT)
 #define L_QWE TO(_QWERTY)
@@ -50,42 +145,45 @@ enum custom_keycodes {
 #define C_TAB C(KC_TAB)
 
 #define SLOT_1 TO(_QWERTY)
-#define SLOT_2 _______
+#define SLOT_2 TO(_LEAGUE)
 #define SLOT_3 _______
 #define SLOT_4 _______
 #define SLOT_5 _______
 #define SLOT_6 _______
 
-// combos
-
-enum combo_events {
-  JUMP_UP,
-  JUMP_DOWN,
-};
-
-const uint16_t PROGMEM ctrl_up[] = {KC_LCTL, KC_UP, COMBO_END};
-const uint16_t PROGMEM ctrl_down[] = {KC_LCTL, KC_DOWN, COMBO_END};
-
-combo_t key_combos[] = {
-  [JUMP_UP] = COMBO_ACTION(ctrl_up),
-  [JUMP_DOWN] = COMBO_ACTION(ctrl_down),
-};
-
-enum unicode_names {
-    BANG,
-    IRONY,
-    SNEK,
-    E_HUN
-};
-
-const uint32_t PROGMEM unicode_map[] = {
-    [BANG]  = 0x203D,   // ‽
-    [IRONY] = 0x2E2E,   // ⸮
-    [SNEK]  = 0x1F40D,  // 🐍
-    [E_HUN] = 0x00E9    // é
-};
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    switch (keycode) {
+    case CHAT:
+        if (record->event.pressed) {
+            tap_code(KC_ENT);
+            layer_move(_CHAT);
+        }
+        return false; // Skip all further processing of this key
+
+    case ACHAT:
+        if (record->event.pressed) {
+            tap_code16(S(KC_ENT));
+            layer_move(_CHAT);
+        }
+        return false; // Skip all further processing of this key
+
+    case ENTnLEA:
+        if (record->event.pressed) {
+            tap_code(KC_ENT);
+            layer_move(_LEAGUE);
+        }
+        return false; // Skip all further processing of this key
+
+    case KC_ENT:
+        if (record->event.pressed) {
+            // rgb_matrix_set_color_all(0xFF,  0xFF, 0xFF);
+        }
+        return false;
+
+    default:
+        return true; // Process all other keycodes normally
+    }
 
     // Layer Lock
     if (!process_layer_lock(keycode, record, LLOCK)) { return false; }
@@ -122,6 +220,19 @@ void matrix_scan_user(void) {
     layer_lock_task();
 }
 
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//     switch (get_highest_layer(state)) {
+//     case _LEAGUE:
+//         rgb_matrix_set_color_all(0x00,  0x00, 0xFF);
+//         break;
+
+//     default: //  for any other layers, or the default layer
+//         rgb_matrix_set_color_all(0x00,  0xFF, 0xFF);
+//         break;
+//     }
+//   return state;
+// }
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_DEFAULT] = LAYOUT_planck_mit(
@@ -132,15 +243,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
     KC_F,   KC_B,   KC_K,   KC_L,   KC_J,   SLOT_5, SLOT_6, KC_Z,   KC_W,   KC_DQUO,KC_COMM,KC_DOT,
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-    _______,KC_LGUI,TAB_SYM,KCR_NAV,ENT_MOU,    _______,    BSP_CAP,SPC_SYM,DEL_SPE,KC_ESC, _______
+    KC_PSCR,KC_LGUI,TAB_SYM,KCR_NAV,ENT_CAP,    _______,    BSP_MOU,SPC_SYM,DEL_SPE,KC_ESC, _______
 //-+-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------+
 ),
 
 [_DEFAULT_CAPS] = LAYOUT_planck_mit(
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-    S(KC_X),S(KC_P),S(KC_D),S(KC_M),S(KC_V),_______,_______,KC_HASH,S(KC_Y),S(KC_O),S(KC_U),KC_AT,
+    S(KC_X),S(KC_P),S(KC_D),S(KC_M),S(KC_Q),_______,_______,KC_HASH,S(KC_Y),S(KC_O),S(KC_U),KC_AT,
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-    S(KC_S),S(KC_N),S(KC_T),S(KC_H),S(KC_G),_______,_______,S(KC_G),S(KC_C),S(KC_A),S(KC_E),S(KC_I),
+    S(KC_S),S(KC_N),S(KC_T),S(KC_H),S(KC_V),_______,_______,S(KC_G),S(KC_C),S(KC_A),S(KC_E),S(KC_I),
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
     S(KC_F),S(KC_B),S(KC_K),S(KC_L),S(KC_J),_______,_______,S(KC_Z),S(KC_W),KC_BSLS,KC_GRV, KC_TILD,
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
@@ -174,13 +285,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FUNCTION] = LAYOUT_planck_mit(
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-    RGB_TOG,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,QK_BOOT,
+    KC_F1,  KC_F2,  KC_F3,  KC_F4,  _______,_______,_______,_______,_______,RGB_TOG,QK_RBT, QK_BOOT,
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-    KC_PSCR,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    KC_F5,  KC_F6,  KC_F7,  KC_F8,  _______,_______,_______,_______,_______,_______,_______,_______,
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-    _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
+    KC_F9,  KC_F10, KC_F11, KC_F12, _______,_______,_______,_______,_______,_______,_______,_______,
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
     _______,_______,_______,_______,_______,     LLOCK,     _______,_______,_______,_______,_______
+//-+-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------+
+),
+
+[_SPECIAL] = LAYOUT_planck_mit(
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,_______,_______,_______,_______,_______,_______,U_O3,   U_O2,   U_O1,   U_O4,   U_SS,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,_______,_______,_______,_______,_______,_______,U_A3,   U_A2,   U_A1,   U_E1,   U_I1,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,_______,_______,_______,_______,_______,_______,U_U3,   U_U2,   U_U1,   _______,_______,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,_______,_______,_______,ENT_SCA,     LLOCK,     KC_BSPC,KC_SPC, KC_DEL, _______,_______
+//-+-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------+
+),
+
+[_SPECIAL_CAPS] = LAYOUT_planck_mit(
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,_______,_______,_______,_______,_______,_______,S(U_O3),S(U_O2),S(U_O1),S(U_O4),S(U_SS),
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,_______,_______,_______,_______,_______,_______,S(U_A3),S(U_A2),S(U_A1),S(U_E1),S(U_I1),
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,_______,_______,_______,_______,_______,_______,S(U_U3),S(U_U2),S(U_U1),_______,_______,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,_______,_______,_______,ENT_SCA,     LLOCK,     KC_BSPC,KC_SPC, KC_DEL, _______,_______
 //-+-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------+
 ),
 
@@ -192,9 +327,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
     KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_ENT,
 //-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
-    RGB_TOG,KC_LCTL,KC_LALT,KC_SPC, KC_LGUI,     L_DEF,     KC_ENT, KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT
+    KC_PSCR,KC_LCTL,KC_LALT,KC_ENT, KC_LGUI,     L_DEF,     KC_BSPC,KC_SPC, KC_DEL, KC_RCTL,KC_RALT
 //-+-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------+
-)
+),
+
+[_LEAGUE] = LAYOUT_planck_mit(
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   _______,_______,_______,_______,_______,_______,_______,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    KC_Q,   KC_W,   KC_E,   KC_R,   KC_LCTL,_______,_______,KC_ENT, _______,_______,_______,_______,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    KC_6,   KC_7,   KC_D,   KC_F,   KC_LSFT,_______,_______,_______,_______,_______,_______,_______,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    KC_F12, _______,KC_TAB, KC_SPC, CHAT,       L_DEF,     _______,_______,_______,_______,_______
+//-+-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------+
+),
+
+[_CHAT] = LAYOUT_planck_mit(
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    KC_X,   KC_P,   KC_D,   KC_M,   KC_Q,   SLOT_1, SLOT_2, KC_AMPR,KC_Y,   KC_O,   KC_U,   KC_QUOT,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    KC_S,   KC_N,   KC_T,   KC_H,   KC_V,   SLOT_3, SLOT_4, KC_G,   KC_C,   KC_A,   KC_E,   KC_I,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    KC_F,   KC_B,   KC_K,   KC_L,   KC_J,   SLOT_5, SLOT_6, KC_Z,   KC_W,   KC_DQUO,KC_COMM,KC_DOT,
+//-+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+
+    _______,KC_LGUI,TAB_SYM,KCR_NAV,ENTnLEA,     L_DEF,     BSP_MOU,SPC_SYM,DEL_SPE,KC_ESC, _______
+//-+-------+-------+-------+-------+-------+---------------+-------+-------+-------+-------+-------+
+),
 
 };
 
